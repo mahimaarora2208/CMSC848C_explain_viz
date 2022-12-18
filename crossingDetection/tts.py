ada8b4e1048750ebb5a5acbc4d715de224401c1d
mahimaarora2208 <marora1@umd.edu>
2022-12-18 14:22:35 -0500

    scenario 1 audio files added

--- crossingDetection/tts.py
+++ crossingDetection/tts.py
@@ -1,12 +1,93 @@
 import gtts
 from playsound import playsound
 
+################################
+# SCENARIO 1 (images 7, 9, 2)
+################################
+baselineStr = "img9_baseline.mp3"
+fivecropStr = "img9_fivecrop.mp3"
+manualStr = "img9_manual.mp3"
 
 # make request to google to get synthesis
-tts = gtts.gTTS("There is a crossing ahead with walk sign on but there is an ambulance coming on the road.")
 
-# save the audio file
-tts.save("hello.mp3")
+# # baseline 7
+# tts = gtts.gTTS("this is a crosswalk")
+# tts.save(baselineStr)
 
-# play the audio file
-playsound("hello.mp3")
+# # five crop 7
+# tts = gtts.gTTS("a wall on the side of a building.power lines above the street.the road is tarmacked.white arrow painted on the street.white lines on a road.")
+# tts.save(fivecropStr)
+ 
+# # manual 7
+# tts = gtts.gTTS("a sign on a pole.")
+# tts.save(manualStr)
+
+# # baseline 2
+# tts = gtts.gTTS("a wall on the side of a building")
+# tts.save(baselineStr)
+
+# # five crop 2
+# tts = gtts.gTTS("a tree in a city. a wall on the side of a building. this is a crosswalk.")
+# tts.save(fivecropStr)
+ 
+# # manual 2
+# tts = gtts.gTTS("the light is orange")
+# tts.save(manualStr)
+
+# baseline 9
+# tts = gtts.gTTS("this is a crosswalk")
+# tts.save(baselineStr)
+
+# # five crop 9
+# tts = gtts.gTTS(" a wall on the side of a building. white line on road. this is a crosswalk.")
+# tts.save(fivecropStr)
+ 
+# # manual 9
+# tts = gtts.gTTS("a city at night")
+# tts.save(manualStr)
+
+
+# playsound("hello.mp3")
+################################
+# SCENARIO 2 (images 6, 8, 3)
+################################
+
+# # baseline 7
+# tts = gtts.gTTS("this is a crosswalk")
+# tts.save(baselineStr)
+
+# # five crop 7
+# tts = gtts.gTTS("a wall on the side of a building.power lines above the street.the road is tarmacked.white arrow painted on the street.white lines on a road.")
+# tts.save(fivecropStr)
+ 
+# # manual 7
+# tts = gtts.gTTS("a sign on a pole.")
+# tts.save(manualStr)
+
+# # baseline 2
+# tts = gtts.gTTS("a wall on the side of a building")
+# tts.save(baselineStr)
+
+# # five crop 2
+# tts = gtts.gTTS("a tree in a city. a wall on the side of a building. this is a crosswalk.")
+# tts.save(fivecropStr)
+ 
+# # manual 2
+# tts = gtts.gTTS("the light is orange")
+# tts.save(manualStr)
+
+# baseline 9
+# tts = gtts.gTTS("this is a crosswalk")
+# tts.save(baselineStr)
+
+# # five crop 9
+# tts = gtts.gTTS(" a wall on the side of a building. white line on road. this is a crosswalk.")
+# tts.save(fivecropStr)
+ 
+# # manual 9
+# tts = gtts.gTTS("a city at night")
+# tts.save(manualStr)
+
+################################
+# SCENARIO 2  (images 4, 5, 1)
+################################
