862abfe9ce56e91ed295643ffbf62cfc4e4d22eb
mahimaarora2208 <marora1@umd.edu>
2022-12-18 22:34:19 -0500

    changed file 3

--- crossingDetection/tts.py
+++ crossingDetection/tts.py
@@ -4,9 +4,9 @@ from playsound import playsound
 ################################
 # SCENARIO 1 (images 7, 9, 2)
 ################################
-baselineStr = "img1_baseline.mp3"
-fivecropStr = "img1_fivecrop.mp3"
-manualStr = "img1_manual.mp3"
+baselineStr = "img3_baseline.mp3"
+fivecropStr = "img3_fivecrop.mp3"
+manualStr = "img3_manual.mp3"
 
 # make request to google to get synthesis
 
@@ -76,17 +76,17 @@ manualStr = "img1_manual.mp3"
 # tts = gtts.gTTS("a wall on the side of the building")
 # tts.save(manualStr)
 
-# # baseline 3
-# tts = gtts.gTTS("this is a crosswalk")
-# tts.save(baselineStr)
+# baseline 3
+tts = gtts.gTTS("white lines on the road")
+tts.save(baselineStr)
 
-# # five crop 3
-# tts = gtts.gTTS("the sky is dark. a wall on the side of a building. the ground is gray. the road is tarmacked")
-# tts.save(fivecropStr)
+# five crop 3
+tts = gtts.gTTS("the sky is dark. a wall on the side of a building. the ground is gray. the road is tarmacked")
+tts.save(fivecropStr)
  
-# # manual 3
-# tts = gtts.gTTS("white lines on the road")
-# tts.save(manualStr)
+# manual 3
+tts = gtts.gTTS("the light is red")
+tts.save(manualStr)
 
 ################################
 # SCENARIO 3  (images 4, 5, 1)
@@ -116,14 +116,14 @@ manualStr = "img1_manual.mp3"
 # tts = gtts.gTTS("the car is parked")
 # tts.save(manualStr)
 
-# baseline 1
-tts = gtts.gTTS("a scene during the day time")
-tts.save(baselineStr)
+# # baseline 1
+# tts = gtts.gTTS("a scene during the day time")
+# tts.save(baselineStr)
 
-# five crop 1
-tts = gtts.gTTS("a wall on the side of a building. the sky is cloudy. crosswalk in the street. white lines on the street")
-tts.save(fivecropStr)
+# # five crop 1
+# tts = gtts.gTTS("a wall on the side of a building. the sky is cloudy. crosswalk in the street. white lines on the street")
+# tts.save(fivecropStr)
  
-# manual 1
-tts = gtts.gTTS("a crosswalk sign")
-tts.save(manualStr)
+# # manual 1
+# tts = gtts.gTTS("a crosswalk sign")
+# tts.save(manualStr)
